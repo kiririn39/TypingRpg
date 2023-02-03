57fef5f71d69fe5e6a4c43db9ec0587f9189f19b
dantselish <dantselish0@gmail.com>
2023-02-03 17:24:33 +0200

    scale to fade

--- Assets/Scripts/Menu/MenuScreen.cs
+++ Assets/Scripts/Menu/MenuScreen.cs
@@ -7,18 +7,20 @@ namespace Menu
     public class MenuScreen : MonoBehaviour
     {
         [SerializeField] private RectTransform rectTransform;
+        [SerializeField] private CanvasGroup   canvasGroup;
 
         private StageManager stageManager;
 
 
         public void startGame()
         {
+            canvasGroup.blocksRaycasts = false;
             // ui_panel_runes_go ??= FindObjectOfType<UIPanelRunes>(true).gameObject;
             // ui_panel_runes_go.SetActive(true);
             stageManager ??= FindObjectOfType<StageManager>();
             stageManager.prepareGame();
             Sequence sequence = DOTween.Sequence();
-            sequence.Append(rectTransform.DOScale(0.0f, 2.0f).SetEase(Ease.InQuart));
+            sequence.Append(canvasGroup.DOFade(0.0f, 2.0f).SetEase(Ease.InQuart));
             sequence.AppendCallback((() => {
                 stageManager.startGame();
             }));
@@ -26,7 +28,8 @@ namespace Menu
 
         public void showAfterLost()
         {
-            rectTransform.localScale = Vector3.one;
+            canvasGroup.blocksRaycasts = true;
+            canvasGroup.alpha = 1.0f;
             rectTransform.anchoredPosition = new Vector2(2560, rectTransform.anchoredPosition.y);
             rectTransform.DOAnchorPosX(0, 2.0f).SetEase(Ease.InOutQuad);
         }
